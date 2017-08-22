486efd898ce4977fb5235446d7f3322358b9fbfd
Kelsey Langlois and Safiy Cham <safiy@>
2017-08-22 11:30:34 -0700

    Handle multiple accounts.

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,8 @@ Account.prototype.withdraw = function(funds) {
   this.balance -= funds;
 }
 
-var newAccount;
+var accounts = {};
+
 // UI Logic
 function clearForm() {
   $("input").val("");
@@ -25,21 +26,26 @@ $(document).ready(function() {
     var initialAmount = parseInt($("input#initial-amount").val());
 
     newAccount = new Account(accountName, initialAmount);
-    console.log(newAccount);
+    accounts[accountName] = newAccount;
+    $(".name").text(accountName);
+    $(".balance").text(newAccount.balance);
+    $(".results").show();
     clearForm();
   });
 
   $("form#change-funds").submit(function(event) {
     event.preventDefault();
+    var accountName = $("input#account").val();
     var depositAmount = parseInt($("input#deposit-amount").val());
     var withdrawalAmount = parseInt($("input#withdrawal-amount").val());
     if (depositAmount) {
-      newAccount.deposit(depositAmount);
+      accounts[accountName].deposit(depositAmount);
     }
     if (withdrawalAmount) {
-      newAccount.withdraw(withdrawalAmount);
+      accounts[accountName].withdraw(withdrawalAmount);
     }
-    console.log(newAccount.balance);
+    $(".name").text(accountName);
+    $(".balance").text(accounts[accountName].balance);
     clearForm();
   });
 });
