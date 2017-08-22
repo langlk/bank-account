aee4321251798dc3d258bedfc067e2ff1429d959
Kelsey Langlois and Safiy Cham <safiy@>
2017-08-22 14:03:00 -0700

    Add comments and tests for valid input.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,17 @@ function Account(userName, initialAmount) {
   this.balance = initialAmount;
 }
 
+// Add funds to an Account's balance
 Account.prototype.deposit = function(funds) {
   this.balance += funds;
 }
 
+// Subtract funds from an Account's balance
 Account.prototype.withdraw = function(funds) {
   this.balance -= funds;
 }
 
+// Our object to store all our Accounts
 var accounts = {};
 
 // UI Logic
@@ -20,33 +23,55 @@ function clearForm() {
 }
 
 $(document).ready(function() {
+  // Event listener for creating new account
   $("form#new-account").submit(function(event) {
     event.preventDefault();
+    // Get user input and store in variables
     var accountName = $("input#account-name").val();
     var initialAmount = parseInt($("input#initial-amount").val());
-    newAccount = new Account(accountName, initialAmount);
-    accounts[accountName] = newAccount;
+    // Check that initialAmount is a number >= 0
+    if (initialAmount >= 0) {
+      // Make a new Account object with this information
+      newAccount = new Account(accountName, initialAmount);
+      // Store this Account in our list of accounts
+      accounts[accountName] = newAccount;
 
-    $(".name").text(accountName);
-    $(".balance").text(newAccount.balance);
-    $(".results").show();
+      // Update page to show transaction information
+      $(".name").text(accountName);
+      $(".balance").text(newAccount.balance);
+      $(".results").show();
+    } else {
+      alert("Please enter a valid amount.")
+    }
+    // Clear form
     clearForm();
   });
 
+  // Event listener for depositing/withdrawing from an account
   $("form#change-funds").submit(function(event) {
     event.preventDefault();
+    // Get user input and store in variables: Account to change, amounts to deposit or withdraw
     var accountName = $("input#account").val();
     var depositAmount = parseInt($("input#deposit-amount").val());
     var withdrawalAmount = parseInt($("input#withdrawal-amount").val());
-    if (depositAmount) {
-      accounts[accountName].deposit(depositAmount);
+    // See if account actually exists
+    if (accounts.hasOwnProperty(accountName)) {
+      // If we have a deposit amount, deposit it
+      if (depositAmount) {
+        accounts[accountName].deposit(depositAmount);
+      }
+      // If we have a withdrawal amount, withdraw it
+      if (withdrawalAmount) {
+        accounts[accountName].withdraw(withdrawalAmount);
+      }
+
+      // Update page to show transaction information
+      $(".name").text(accountName);
+      $(".balance").text(accounts[accountName].balance);
+    } else {
+      alert("No such account.");
     }
-    if (withdrawalAmount) {
-      accounts[accountName].withdraw(withdrawalAmount);
-    }
-    
-    $(".name").text(accountName);
-    $(".balance").text(accounts[accountName].balance);
+    // Clear form
     clearForm();
   });
 });
